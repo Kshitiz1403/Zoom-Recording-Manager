bc2902ece15d6e657d6396e122836f63a5e3788a
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2024-09-18 20:37:41 +0530

    feat: send mail to self aswell

--- send-email.js
+++ send-email.js
@@ -5,15 +5,14 @@ import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = async (presignedURL) => {
- const msg = {
+ sgMail.sendMultiple()
+ const sent = await sgMail.send({
   from: "zoom@kshitizagrawal.in",
-  to: "sanjeev@dreamsoft4u.com",
+  to: ["sanjeev@dreamsoft4u.com", "kshitizagrawal@outlook.com"],
   subject: "Your Zoom Download is Ready",
-  text: "Your Zoom Download is Ready, Click the link below to download it \n" + presignedURL,
-  html: "<strong>Your Zoom Download is Ready, Click the link below to download it</strong> <br> <a href='" + presignedURL + "'>Download</a>"
- }
-
- const sent = await sgMail.send(msg)
+  text: `Your Zoom Download is Ready, Click the link below to download it \n" + ${presignedURL}. The link is only valid for 7 days.`,
+  html: "<strong>Your Zoom Download is Ready, Click the link below to download it</strong> <br> <a href='" + presignedURL + "'>Download</a> <br> The link is only valid for 7 days."
+ })
 
  console.log("Email sent")
 
