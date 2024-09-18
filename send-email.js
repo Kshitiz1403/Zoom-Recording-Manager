6df39504a42476885e635d34a87ef7f399f9fce7
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2024-09-18 20:51:15 +0530

    fix: multiple email send

--- send-email.js
+++ send-email.js
@@ -5,31 +5,17 @@ import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = async (presignedURL) => {
- sgMail.sendMultiple()
- let sent = await sgMail.send({
+ const sent = await sgMail.send({
   from: "zoom@kshitizagrawal.in",
-  to: ["sanjeev@dreamsoft4u.com"],
+  to: ["sanjeev@dreamsoft4u.com", "kshitizagrawal@outlook.com"],
   subject: "Your Zoom Download is Ready",
   text: `Your Zoom Download is Ready, Click the link below to download it \n" + ${presignedURL}. \nThe link is only valid for 7 days.`,
   html: "<strong>Your Zoom Download is Ready, Click the link below to download it</strong> <br> <a href='" + presignedURL + "'>Download</a> <br> The link is only valid for 7 days."
- })
+ }, true)
 
  console.log("Email sent")
 
  console.log(sent)
- 
- sent = await sgMail.send({
-  from: "zoom@kshitizagrawal.in",
-  to: ["kshitizagrawal@outlook.com"],
-  subject: "Your Zoom Download is Ready",
-  text: `Your Zoom Download is Ready, Click the link below to download it \n" + ${presignedURL}. \nThe link is only valid for 7 days.`,
-  html: "<strong>Your Zoom Download is Ready, Click the link below to download it</strong> <br> <a href='" + presignedURL + "'>Download</a> <br> The link is only valid for 7 days."
- })
- 
-  console.log("Email sent")
-  
-  console.log(sent)
-  
  return sent
 }
 
